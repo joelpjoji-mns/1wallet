3ed520d7e05d996a0c746aabd84ecc2a2c3fef42
Joel P Joji <143604254+joelpjoji-mns@users.noreply.github.com>
2026-06-12 00:19:03 +0100

    fix: resolve Category naming collision from foundation.dart

--- lib/src/data/ledger_providers.dart
+++ lib/src/data/ledger_providers.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' show compute;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
