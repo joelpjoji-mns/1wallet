855319f4b722806b362003df37c86726efcec65a
Joel P Joji <143604254+joelpjoji-mns@users.noreply.github.com>
2026-06-18 20:43:20 +0100

    fix(auth): fix biometrics and ensure clean build

--- lib/src/features/accounts/account_editor_screen.dart
+++ lib/src/features/accounts/account_editor_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:local_auth/local_auth.dart';
 
 import '../../data/ledger_models.dart';
 import '../../data/ledger_providers.dart';
